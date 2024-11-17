e1562663d82adb290b55f2a0077e3ea0a8e6fa6e
robo-shreyansh <shreyansh.sh99@gmail.com>
2024-11-17 06:42:29 +0530

    Performing cross validation

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 from feature_extraction import FeatureExtractor
 
+random.seed(0)
 
 class Dataset:
     def __init__(self, filepath=None):
@@ -13,7 +14,6 @@ class Dataset:
             data = csv.reader(csvfile, delimiter=',')
             data_matrix = []
             for row in data:
-                #convert to features here! 
                 data_vec = [float(x) for x in row[:-1]]
                 label = int(row[-1])
                 train_feats = FeatureExtractor(data_vec,label)
@@ -31,6 +31,25 @@ class Dataset:
         self.test_data = self.data_matrix[int(train_len*len(self.labels)):, :]
         self.test_labels = self.labels[int(train_len*len(self.labels)):]
 
+    def get_cross_validation_folds(self, num_folds=5, fold_len=0.75):
+        
+        dataset_len = len(self.data_matrix)
+
+        test_data = self.data_matrix[int(dataset_len*fold_len): , :]
+        test_label = self.labels[int(dataset_len*fold_len):]
+
+        data_folds = []
+        data_folds_labels = []
+        for i in range(num_folds): 
+            start_idx = int(dataset_len * i * fold_len / num_folds)
+            end_idx = int(dataset_len * (i + 1) * fold_len / num_folds)
+            
+            data_folds.append(self.data_matrix[start_idx:end_idx, :])
+            data_folds_labels.append(self.labels[start_idx:end_idx])
+
+
+        return data_folds, data_folds_labels, test_data, test_label
+    
     def get_train_data(self):
         return self.train_data, self.train_labels
     
@@ -44,26 +63,34 @@ class KNN:
     def __init__(self, k=0):
         self.k = k
         self.neighbours=None
+        self.eval = True
 
     def load_data(self,data,label):
         self.data = data
         self.label = label
 
+    def evaluate(self, eval = True):
+        self.eval=eval
+
     def predict(self,x):
-        self.feature_vec = x
+        if self.eval:
+            self.input_feature_vec=x
+        else:
+            self.input_feature_vec = FeatureExtractor(x)
         self.find_nearest_neighbours()
+
+    def set_distance(self, dist_name):
+        if dist_name=="euclidean":
+            self.dist_func = lambda x,y : np.linalg.norm(x-y, ord=2)
+        elif dist_name=="manhattan":
+            self.dist_func = lambda x,y : np.linalg.norm(x-y, ord=1)
 
     def find_nearest_neighbours(self):
         dist_list = []
-        for point in self.data:
-            dist = 0
-            for i in range(len(point)):
-                dist+= (point[i] - self.feature_vec[i])**2
-            dist_list.append( dist**0.5 )
-        
-        distance = list(zip(dist_list, self.label))
-        distance.sort(key= lambda x : x[0])
-        self.neighbours = [distance[k][1] for k in range(self.k)]
+        dist_list = [self.dist_func(point, self.input_feature_vec) for point in self.data]
+        distance = sorted(zip(dist_list, self.label), key=lambda x: x[0])
+        self.neighbours = [distance[i][1] for i in range(self.k)]
+
     
     def get_label(self):
         return max(set(self.neighbours), key= self.neighbours.count)
@@ -95,16 +122,3 @@ class LogisticRegression():
             probabilities.append(np.exp(score)/np.sum(scores))
         
         return max(probabilities) # index of this is the answer. 
-    pass
-
-
-path = "vbh_dataset.csv"
-dataset = Dataset(path)
-dataset.load_data()
-dataset.train_test_split(0.95)
-train_data, train_labels = dataset.get_train_data()
-test_data, test_labels = dataset.get_test_data()
-data, labs = dataset.get_data()
-knn = KNN(k = 3)
-knn.load_data(train_data, train_labels)
-knn.load_data(data, labs)
--- train_test.py
+++ train_test.py
@@ -1,77 +1,111 @@
-# Load the data, create a split, and return the particular sample. 
-# TO DO: 
-# Build / Load model
-# Train and test
-# Save in whichever format#
-
-import csv
 from models import Dataset, KNN
 import numpy as np
-import random
-import cv2
-from statistics import mode
+import matplotlib.pyplot as plt
 
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-
-# Declaring the frame width and height
-FRAME_WIDTH=640
-FRAME_HEIGHT=480
-
-
-# Medipipe hand landmark detector options
-base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
-options = vision.HandLandmarkerOptions(base_options=base_options,
-                                       num_hands=1,
-                                       min_hand_detection_confidence=0.4)
-detector = vision.HandLandmarker.create_from_options(options)
-
-from feature_extraction import FeatureExtractor
 
 if __name__=="__main__":
-    FRAME_WIDTH=640
-    FRAME_HEIGHT=480
+
     path = "vbh_dataset.csv"
     dataset = Dataset(path)
     dataset.load_data()
-    dataset.train_test_split(0.95)
-    train_data, train_labels = dataset.get_train_data()
-    test_data, test_labels = dataset.get_test_data()
-    knn = KNN(k = 3)
-    knn.load_data(train_data, train_labels)
-    webcam = cv2.VideoCapture(0)
-    webcam.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
-    webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
-    num_samp = 0
-    points_of_interest = [1,5,9,13,17, 4,8,12,16,20]
-    # create / open vbh_dataset.csv
+    num_folds = 5
+    fold_len = 0.75
+    folds, folds_labels, test, test_labels = dataset.get_cross_validation_folds(num_folds=num_folds, fold_len=fold_len)
 
-    while(True):
-        ret, frame = webcam.read()
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-        hand_landmarker_result = detector.detect(mp_image)
-        hand_landmarks = hand_landmarker_result.hand_landmarks
-        points = []
-        lab = ""
-        if (len(hand_landmarks)!=0):
-            points = []
-            for point in points_of_interest:
-                x = hand_landmarks[0][point].x - hand_landmarks[0][0].x
-                y = hand_landmarks[0][point].y - hand_landmarks[0][0].y
-                points.append(x)
-                points.append(y)
-            feats = FeatureExtractor(points)
-            knn.predict(x = feats)
-            print(mode(knn.neighbours))
-        cv2.imshow('WebCam', frame)
-        cv2.waitKey(50)
-        if cv2.waitKey(1) & 0xFF==ord('q'):
-            break
+    epoch_accuracy = []
+    epoch_mean_accuracy=[]
+    k_vals = []
 
-    webcam.release()
-    cv2.destroyAllWindows()
+    for k in range(2,16):
 
-    knn.load_data(train_data, train_labels)
-    knn.predict(test_data[5])
-    print(knn.neighbours)
+        knn = KNN(k = k)
+        knn.set_distance("euclidean")
+        accuracy = []
+        
+        for i in range(num_folds):
+
+            val = folds[i]
+            val_labels = folds_labels[i]
+
+            train_data = np.vstack([folds[j] for j in range(num_folds) if j != i])
+            train_labels = np.hstack( [folds_labels[j] for j in range(num_folds) if j != i] )
+            knn.load_data(train_data, train_labels)
+            output=[]
+
+            for val_point in val:
+                knn.predict(val_point)
+                output.append( knn.get_label() )
+            output = np.array(output)
+            accuracy.append( np.sum((output==val_labels))/len(output) )
+        
+        print("Checking for k = ",k)
+
+        mean_accuracy = sum(accuracy)/len(accuracy)
+        k_vals.append(k)
+        epoch_accuracy.append(accuracy)
+        epoch_mean_accuracy.append(mean_accuracy)
+        
+    plt.figure(figsize=(8, 6))
+    plt.plot(k_vals, epoch_mean_accuracy, marker='o', linestyle='-', color='b', label='Euclidean Distance')
+
+    plt.xlabel('Number of Neighbors (k)')
+    plt.ylabel('Mean Accuracy')
+    plt.title('KNN Performance vs. Number of Neighbors (k)')
+    plt.legend()
+
+    plt.grid(True)
+    plt.show()
+
+
+
+#import mediapipe as mp
+#from mediapipe.tasks import python
+#from mediapipe.tasks.python import vision
+# 
+# Medipipe hand landmark detector options
+# base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
+# options = vision.HandLandmarkerOptions(base_options=base_options,
+#                                        num_hands=1,
+#                                        min_hand_detection_confidence=0.4)
+# detector = vision.HandLandmarker.create_from_options(options)
+    # # Declaring the frame width and height
+# FRAME_WIDTH=640
+# FRAME_HEIGHT=480
+    # train_data, train_labels = dataset.get_train_data()
+    # test_data, test_labels = dataset.get_test_data()
+    # knn = KNN(k = 3)
+    # knn.load_data(train_data, train_labels)
+    # FRAME_WIDTH=640
+    # FRAME_HEIGHT=480
+    # webcam = cv2.VideoCapture(0)
+    # webcam.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
+    # webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
+    # num_samp = 0
+    # points_of_interest = [1,5,9,13,17, 4,8,12,16,20]
+    # # create / open vbh_dataset.csv
+    # while(True):
+    #     ret, frame = webcam.read(_of_interest:
+    #             x = hand_landmarks[0][point].x - hand_landmarks[0][0].x
+    #             y = hand_landmarks[0][point].y - hand_landmarks[0][0].y
+    #             points.append(x)
+    #             points.append(y)
+    #         feats = FeatureExtractor(points)
+    #         knn.predict(x = feats)
+    #         print(mode(knn.neighbours))
+    #     cv2.imshow('WebCam', frame)
+    #     cv2.waitKey(50)
+    #     if cv2.waitKey(1) & 0xFF==ord('q'):
+    #         break
+    # webcam.release()
+    # cv2.destroyAllWindows()
+    # knn.load_data(train_data, train_labels)
+    # knn.predict(test_data[5])
+    # print(knn.neighbours))
+    #     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+    #     hand_landmarker_result = detector.detect(mp_image)
+    #     hand_landmarks = hand_landmarker_result.hand_landmarks
+    #     points = []
+    #     lab = ""
+    #     if (len(hand_landmarks)!=0):
+    #         points = []
+    #         for point in points_
